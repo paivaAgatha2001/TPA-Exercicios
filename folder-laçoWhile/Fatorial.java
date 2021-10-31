9e6ea515db6c0c6c30c5b425e56d42f9ff328e77
Agatha <92937531+paivaAgatha2001@users.noreply.github.com>
2021-10-31 15:51:35 -0300

    Update Fatorial.java

--- folder-laçoWhile/Fatorial.java
+++ folder-laçoWhile/Fatorial.java
@@ -4,7 +4,7 @@ public class Fatorial {
 		Scanner in = new Scanner(System.in);
 		int n, f = 1, i = 1 ; 
 		
-		System.out.println("Digite um número:");
+		System.out.println("Digite um nÃºmero:");
 		n = in.nextInt();
 		
 		while ( i <= n ) {
@@ -12,20 +12,7 @@ public class Fatorial {
 			i++;
 			
 		}
-		System.out.println("A fatorial do número é: " +f);
+		System.out.println("A fatorial do nÃºmero Ã©: " +f);
 		in.close();
 	}
 }
-
-//public class Fatorial {
-//public static void main(String[] args) {
-//double i = 5;
-//double f = i;
-
-//while ( i > 1 ) {
-	//f = f* (i-1);
-	//i--;
-	
-//}
-//System.out.println(f);
-//}
