81dfde9af42ba55000c5b7a1912f69c57dda4eca
Agatha <92937531+paivaAgatha2001@users.noreply.github.com>
2021-11-02 17:07:32 -0300

    Add files via upload

---
+++ folder-PrimeirosProgramas/SomaValores.java
@@ -0,0 +1,11 @@
+public class SomaValores {
+  public static void main (String args[]) {
+	  int n1, n2, soma;
+	  
+	  n1 = 400;
+	  n2 = 	100;
+	  soma = n1+n2;
+	  
+	  System.out.println("A soma de "+n1+" + "+n2+ " = "+soma);
+  }
+}
